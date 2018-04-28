a0b9a0863f116806b736c301f706fc63e43d83a0
Kayan Azimov <kayan.subscriber@gmail.com>
2018-04-28 21:01:05 +0100

    use custom ip instead of dynamic

--- 8-ec2-in-vpc-with-remote-state-new-structure/service/config.tf
+++ 8-ec2-in-vpc-with-remote-state-new-structure/service/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "kayan-terra-state"
-    key    = "dev/service/terraform.tfstate"
+    key    = "dev/ec2/terraform.tfstate"
     region = "eu-west-2"
   }
 }
--- 8-ec2-in-vpc-with-remote-state-new-structure/service/main.tf
+++ 8-ec2-in-vpc-with-remote-state-new-structure/service/main.tf
@@ -5,6 +5,8 @@ resource "aws_instance" "front_ec2" {
   instance_type = "t2.micro"
   subnet_id     = "${data.terraform_remote_state.vpc.vpc_subnet_front_id}"
 
+  private_ip = "10.0.1.100"
+
   tags {
     Name = "${var.tag-name}-example ec2 front"
   }
@@ -14,6 +16,8 @@ resource "aws_instance" "back_ec2" {
   ami           = "ami-a1f5e4c5"
   instance_type = "t2.micro"
   subnet_id     = "${data.terraform_remote_state.vpc.vpc_subnet_back_id}"
+
+  private_ip = "10.0.2.100"
 
   tags {
     Name = "${var.tag-name}-example ec2 back"
