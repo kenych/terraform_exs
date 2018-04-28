5204b665fbc17d8c2b12b027c1363afc8f12d6a1
Kayan Azimov <kayan.subscriber@gmail.com>
2018-04-28 18:52:31 +0100

    use git module

---
+++ 9-use_module_git/init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+yum install perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https -y
+mkdir /CloudWatch
+cd /CloudWatch
+curl http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip -O
+unzip CloudWatchMonitoringScripts-1.2.1.zip
+rm -rf CloudWatchMonitoringScripts-1.2.1.zip
+cd aws-scripts-mon
+./mon-put-instance-data.pl --mem-util --verify --verbose > CloudWatchtest.log
+(crontab -l 2>/dev/null; echo "* * * * * /CloudWatch/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --disk-space-util --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail") | crontab -
---
+++ 9-use_module_git/main.tf
@@ -0,0 +1,28 @@
+module "iam_role_for_ec2_to_cloud_watch" {
+  source = "github.com/kenych/terraform_module_iam_role.git"
+  assume_role_policy = "${file("policies/aws_iam_assume_role_ec2.json")}"
+  aws_iam_role_policy = "${file("policies/aws_iam_role_policy_cloud_watch.json")}"
+  role_purpose = "${var.role_purpose}"
+}
+
+resource "aws_instance" "centos" {
+  ami = "ami-dff017b8"
+  instance_type = "t2.micro"
+  key_name = "terraform_ec2_key"
+
+  tags {
+    Name = "helloTerraformCloudWatch"
+  }
+
+  iam_instance_profile = "iam_instance_profile_m_${var.role_purpose}"
+
+  key_name = "terra"
+
+  user_data = "${file("init.sh")}"
+
+  provisioner "local-exec" {
+    command = "echo Public IP: ${self.public_ip}"
+  }
+
+}
+
