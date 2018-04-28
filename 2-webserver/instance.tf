7529aa3afad5842dbe29a1007db1d1dbb05ddc31
Kayan Azimov <kayan.subscriber@gmail.com>
2018-04-28 20:28:10 +0100

    use cloud-init config

--- 2-webserver/instance.tf
+++ 2-webserver/instance.tf
@@ -39,6 +39,46 @@ resource "aws_instance" "my_ec2" {
   }
 }
 
+resource "aws_instance" "my_ec2_2" {
+  //  amazon linux ami
+  ami           = "${lookup(var.distros, "amazon")}"
+  instance_type = "t2.micro"
+
+  key_name = "terra"
+
+  user_data = <<-EOF
+    #cloud-config
+    repo_update: true
+    repo_upgrade: all
+
+    packages:
+     - httpd24
+     - php56
+     - mysql55-server
+     - php56-mysqlnd
+
+    runcmd:
+     - service httpd start
+     - chkconfig httpd on
+     - groupadd www
+     - [ sh, -c, "usermod -a -G www ec2-user" ]
+     - [ sh, -c, "chown -R root:www /var/www" ]
+     - chmod 2775 /var/www
+     - [ find, /var/www, -type, d, -exec, chmod, 2775, {}, + ]
+     - [ find, /var/www, -type, f, -exec, chmod, 0664, {}, + ]
+     - [ sh, -c, 'echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php' ]
+    EOF
+
+  //  implicit dep
+  vpc_security_group_ids = [
+    "${aws_security_group.web_app_sg.id}",
+  ]
+
+  tags {
+    Name = "example ec2 with web app and cloud init"
+  }
+}
+
 variable "web_server_port" {
   description = "http port"
   default     = 80
